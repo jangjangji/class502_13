4f9682e20e0ff36ec0fe20bcecfdef88961c0cdb
jangjangji <sjjang41@naver.com>
2024-04-12 16:24:50 +0900

    변수 기초 공부

--- 응용 SW 기초 기술활용/source/day01_2/src/Ex12.java
+++ 응용 SW 기초 기술활용/source/day01_2/src/Ex12.java
@@ -2,7 +2,7 @@ public class Ex12 {
     public static void main(String[] args) {
         int num1 = 12;
         double num2 = 3.6;
-
+        int num4 =20;
         int num3 = (int)(num1 * num2);
         System.out.println(num3);
     }
