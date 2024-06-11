2adb2e2657b807baf17818234954721bd8a404ef
jangjangji <sjjang41@naver.com>
2024-06-11 16:31:58 +0900

    jsp 회원가입 기본틀 설정

--- Myjava/day03/src/main/java/servlets/HelloServlet.java
+++ Myjava/day03/src/main/java/servlets/HelloServlet.java
@@ -1,5 +1,6 @@
 package servlets;
 
+import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -20,8 +21,22 @@ public class HelloServlet extends HttpServlet {
         System.out.println("destroy()");
     }
 
-    @Override
+   /* @Override
     public void init() throws ServletException {
         System.out.println("init()");
     }
+
+    */
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        String key1 = config.getInitParameter("key1");
+        String key2 = config.getInitParameter("key2");
+        System.out.printf("key1=%s , key2 = %s%n" ,key1,key2);
+        String common1 = config.getInitParameter("common1");
+        String common2 = config.getInitParameter("common2");
+        System.out.printf("com1 =%s, com2 = %s%n",common1,common2);
+
+    }
+
 }
